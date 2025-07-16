1cd77c90b70e99aa0841e126fedf18c1153d24f6
Mario <mosermauer@gmail.com>
2025-07-16 17:03:53 +0200

    added .gitignore and tested blinking for all leds

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,42 @@
 #include <thread>
 #include <chrono>
 
-int main() {
-    constexpr auto chipname = "gpiochip0";
-    constexpr unsigned int line_num = 26;
+constexpr auto chipname = "gpiochip0";
+constexpr unsigned int line_num = 26;
 
-    gpiod::chip chip1(chipname);
-    gpiod::line line16 = chip1.get_line(line_num); //green
+void gpioLed(const std::string& requestName, gpiod::chip& c, gpiod::line& l) {
+    l.request({requestName, gpiod::line_request::DIRECTION_OUTPUT, 0});
 
-    line16.request({"diningPhilosophers",gpiod::line_request::DIRECTION_OUTPUT,0});
-
-    for(auto i = 0 ; i < 20 ; ++i)
-    {
-        if(i % 2 == 0)
-        {
-          line16.set_value(1);
-          std::this_thread::sleep_for(std::chrono::seconds(1));
-        } 
-        else
-        {
-          line16.set_value(0);
-          std::this_thread::sleep_for(std::chrono::seconds(1));
-        } 
+    for (int i = 0; i < 20; ++i) {
+        l.set_value(i % 2);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
- 
-    line16.set_value(0);
 
+    l.set_value(0);
+}
 
-       
+int main() {
+    gpiod::chip chip(chipname);
+    gpiod::line l1 = chip.get_line(22);
+    gpiod::line l2 = chip.get_line(16);
+    gpiod::line l3 = chip.get_line(18);
+    gpiod::line l4 = chip.get_line(19);
+    gpiod::line l5 = chip.get_line(26);
+
+    std::thread t1(gpioLed, "led_request1", std::ref(chip), std::ref(l1));
+    std::thread t2(gpioLed, "led_request2", std::ref(chip), std::ref(l2));
+    std::thread t3(gpioLed, "led_request3", std::ref(chip), std::ref(l3));
+    std::thread t4(gpioLed, "led_request4", std::ref(chip), std::ref(l4));
+    std::thread t5(gpioLed, "led_request5", std::ref(chip), std::ref(l5));
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+
     return 0;
 }
+
+
+
