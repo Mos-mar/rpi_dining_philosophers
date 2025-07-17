21df291cc53e078228add7232a058fb4838f0a9f
Mario <mosermauer@gmail.com>
2025-07-17 18:16:38 +0200

    testing gpioManager with threads and lambdas

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,25 @@
 
 
 
-int main() {
-    
-    
+int main() {   
     GpioManager g1,g2;
-    g1.init_pin(26,"GREEN!");
-    g1.blink(26,3,1000);
 
-    g2.init_pin(19,"YELLOW!");
-    g2.toggle(19);
+    std::thread t1(
+        [&g1](){
+        g1.init_pin(26,"GREEN!");
+        g1.blink(26,3,1000);
+        
+    });
+
+    std::thread t2(
+        [&g2](){
+        g2.init_pin(19,"YELLOW!");
+        g2.toggle(19);
+    });
+
+    t1.join();
+    t2.join();  
     g2.set_low(19);
-    
-    
-    
 
 }
 
